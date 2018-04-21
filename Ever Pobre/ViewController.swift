18f74f5cbce37c06aace6dec7eeb07c18888b98b
Manuel I. Perez Soriano <mperezsoriano@gmail.com>
2018-04-21 18:47:16 +0200

    Initial Commit

---
+++ Ever Pobre/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ever Pobre
+//
+//  Created by Manuel Perez Soriano on 21/4/18.
+//  Copyright © 2018 Manuel Perez Soriano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
